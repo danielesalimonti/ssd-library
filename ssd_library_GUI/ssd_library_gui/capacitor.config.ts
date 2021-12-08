da42a7497e464e3083beb18325884c565e4b40c4
danielesalimonti <danielesalim@outlook.it>
2021-12-08 14:43:04 +0100

    first gui commit

---
+++ ssd_library_GUI/ssd_library_gui/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ssd_library_gui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
