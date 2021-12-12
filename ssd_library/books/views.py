7f39f655d6a0c89e232d413cf996f1ac33e312cf
danielesalimonti <danielesalim@outlook.it>
2021-12-12 14:03:45 +0100

    added more tests and fixed the rent_book rest function

--- ssd_library/books/views.py
+++ ssd_library/books/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import get_user_model
+import json
+
 from django.shortcuts import render, redirect
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 from .models import Book
 from .permissions import AreRentedBook
@@ -9,22 +12,26 @@ from django.http import HttpResponse
 
 
 class BookListAdmin(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookDetail(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookRentedList(generics.ListAPIView):
     serializer_class = BookSerializerForRentedBooks
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Book.objects.filter(_user_rented__contains=self.request.user)
@@ -33,20 +40,22 @@ class BookRentedList(generics.ListAPIView):
 class BookRentedDetail(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializerForRentedBooks
-    permission_classes = [AreRentedBook]
+    permission_classes = [AreRentedBook, IsAuthenticated]
 
 
 class BookRent:
 
     @staticmethod
     def rent_book(request, isbn):
-        book = Book.objects.get(ISBN=isbn)
-        response = HttpResponse()
-        if book is None:
-            response.status_code = 404
-            return response
-        else:
-            response.status_code = 200
+        if request.user.is_authenticated:
+            try:
+                book = Book.objects.get(ISBN=isbn)
+            except Book.DoesNotExist:
+                return HttpResponse(json.dumps({'detail': 'Book not found'}), content_type="application/json",
+                                    status=404)
 
-        book.add_user_rent(str(request.user))
-        return redirect('/../../api/v1/')
+            book.add_user_rent(str(request.user))
+            return redirect('/../../api/v1/')
+
+        return HttpResponse(json.dumps({'detail': 'Authentication credentials were not provided.'}),
+                            content_type="application/json", status=403)
--- ssd_library/tests/books/test_urls.py
+++ ssd_library/tests/books/test_urls.py
@@ -1,7 +1,10 @@
 import json
+import random
+import string
+
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from rest_framework.status import HTTP_403_FORBIDDEN, HTTP_200_OK
+from rest_framework.status import HTTP_403_FORBIDDEN, HTTP_200_OK, HTTP_404_NOT_FOUND
 from rest_framework.test import APIClient
 import pytest
 from mixer.backend.django import mixer
@@ -36,19 +39,23 @@ def contains(response, key, value):
     return value in obj[key]
 
 
-def test_books_get_list(books):
+def test_books_get_list_not_authenticated(books):
     path = reverse('books')
-    print("---------------" + path + "-----------------------")
     client = get_client()
     response = client.get(path)
+    assert response.status_code == HTTP_403_FORBIDDEN
+
+
+def test_books_get_list_authenticated(books):
+    path = reverse('books')
+    user = mixer.blend(get_user_model())
+    client = get_client(user)
+    response = client.get(path)
     assert response.status_code == HTTP_200_OK
-    obj = parse(response)
-    assert len(obj) == len(books)
 
 
 def test_get_non_mine_book(books):
     path = reverse('my-books') + '/' + books[0].ISBN + '/'
-    print("---------------" + path + "-----------------------")
     user = mixer.blend(get_user_model())
     client = get_client(user)
     response = client.get(path)
@@ -56,9 +63,8 @@ def test_get_non_mine_book(books):
     assert response.status_code == HTTP_403_FORBIDDEN
 
 
-def test_get_mine_book(books):
+def test_rent_and_get_mine_book(books):
     path = '/api/v1/rent/' + books[0].ISBN + '/'
-    print("---------------" + path + "-----------------------")
     user = mixer.blend(get_user_model())
     client = get_client(user)
     response = client.get(path)
@@ -66,3 +72,18 @@ def test_get_mine_book(books):
     response = client.get(path)
     print(books[0].user_rented)
     assert response.status_code == HTTP_200_OK
+
+
+def test_rent_book_not_authenticated(books):
+    path = '/api/v1/rent/' + books[0].ISBN + '/'
+    client = get_client()
+    response = client.get(path)
+    assert response.status_code == HTTP_403_FORBIDDEN
+
+
+def test_rent_non_existent_book(books):
+    path = '/api/v1/rent/' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=20)) + '/'
+    user = mixer.blend(get_user_model())
+    client = get_client(user)
+    response = client.get(path)
+    assert response.status_code == HTTP_404_NOT_FOUND
