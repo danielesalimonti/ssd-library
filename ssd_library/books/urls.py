8b5868b2fa94a5818037ab9872a5ebf4e2ef3c5a
danielesalimonti <danielesalim@outlook.it>
2021-12-13 10:50:17 +0100

    fix urls again

--- ssd_library/books/urls.py
+++ ssd_library/books/urls.py
@@ -3,12 +3,10 @@ from django.urls import path
 from .views import BookDetail, BookList, BookRent, BookRentedList, BookListAdmin, BookRentedDetail
 
 urlpatterns = [
-    path('my-books/', BookRentedList.as_view()),
-    path('<str:pk>/', BookDetail.as_view(), name='books'), #libro specifico
-    path('', BookList.as_view(), name='my-books'), #index
-    path('rent/<str:isbn>/', BookRent.as_view()), #noleggio libro
+    path('my-books/', BookRentedList.as_view(), name='my-books'),
+    path('<str:pk>/', BookDetail.as_view(), ),  # libro specifico
+    path('', BookList.as_view(), name='books'),  # index
+    path('rent/<str:isbn>/', BookRent.as_view()),  # noleggio libro
     path('admin/<str:pk>/', BookListAdmin.as_view()),
     path('my-books/<str:pk>/', BookRentedDetail.as_view())
 ]
-
-
