361da53714ef3a332d989e85b58d03946b904ae8
Thomas <thom_96@hotmail.it>
2021-12-16 11:52:18 +0100

    fix db

--- ssd_library/books/models.py
+++ ssd_library/books/models.py
@@ -5,17 +5,12 @@ from django.core.validators import RegexValidator, MinValueValidator, MaxValueVa
 
 
 class Book(models.Model):
-    ISBN = models.CharField(max_length=17, primary_key=True, validators=[RegexValidator(
-        regex=r'^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$', message="Doesn't match ISBN pattern")])
-    author = models.CharField(max_length=50, validators=[RegexValidator(regex=r'^[a-zA-Z ]+$',
-                                                                        message="Invalid author")])
-    title = models.CharField(max_length=50, validators=[RegexValidator(regex=r'^[a-zA-Z0-9 ]+$',
-                                                                       message="Invalid title")])
-    preview = models.CharField(max_length=60, default='', validators=[RegexValidator(regex=r'^[a-zA-Z0-9,\.?!\' ]+$',
-                                                                                     message="Invalid preview")])
+    ISBN = models.CharField(max_length=17, primary_key=True, validators=[RegexValidator(regex=r'^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$', message="Doesn't match ISBN pattern")])
+    author = models.CharField(max_length=50, validators=[RegexValidator(regex=r'^[a-zA-Z ]+$', message="Invalid author")])
+    title = models.CharField(max_length=50, validators=[RegexValidator(regex=r'^[a-zA-Z0-9 ]+$',message="Invalid title")])
+    preview = models.CharField(max_length=60, default='', validators=[RegexValidator(regex=r'^[a-zA-Z0-9,\.?!\' ]+$',message="Invalid preview")])
 
-    text = models.TextField(max_length=1000, validators=[RegexValidator(regex=r'^[a-zA-Z0-9,.?\'! ]+$',
-                                                                        message="Invalid description")])
+    text = models.TextField(max_length=1000, validators=[RegexValidator(regex=r'^[a-zA-Z0-9,.?\'! ]+$',message="Invalid description")])
 
     published_date = models.DateField()
     num_pages = models.IntegerField(validators=[MinValueValidator(limit_value=1), MaxValueValidator(limit_value=10000)])
